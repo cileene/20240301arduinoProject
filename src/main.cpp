3ba02cb0904af45bdf6fb5022cea416f53832d70
cileene <106276740+cileene@users.noreply.github.com>
2024-03-01 11:17:16 +0100

    it's working again

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@ int myFunction(int, int);
 void setup()
 {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Hello World test");
   int result = myFunction(2, 3);
   pinMode(12, OUTPUT);
 }
@@ -15,9 +17,13 @@ void loop()
 {
   // put your main code here, to run repeatedly:
   digitalWrite(12, HIGH);
-  delay(100);
+  delay(500);
   digitalWrite(12, LOW);
-  delay(100);
+  delay(500);
+  Serial.println(millis());
+  // print the time since program started
+  
+  
 }
 
 // put function definitions here:
